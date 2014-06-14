4d4c39c31aafa31caf00d199d21c9552d7a5f24d
Kaho Sato <kahosato93@gmail.com>
2014-06-14 21:37:16 +0100

    [KS] Implementation : Prettier view of pop out

--- src/src/com/webapp/PastContextSelectedFragment.java
+++ src/src/com/webapp/PastContextSelectedFragment.java
@@ -2,15 +2,20 @@ package com.webapp;
 
 import java.util.ArrayList;
 
+import android.app.Dialog;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.View.OnTouchListener;
+import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class PastContextSelectedFragment extends DialogFragment {
@@ -36,7 +41,6 @@ public class PastContextSelectedFragment extends DialogFragment {
                 R.id.title,
                 links,
                 new OnTouchListener() {
-					
 					@Override
 					public boolean onTouch(View arg0, MotionEvent arg1) {
 						return false;
@@ -44,7 +48,26 @@ public class PastContextSelectedFragment extends DialogFragment {
 				});
     	ListView mListView = (ListView) view.findViewById(R.id.links);
         mListView.setAdapter(mAdapter);
-		return view;
+        Button close = (Button) view.findViewById(R.id.close_button);
+		Typeface font = Typeface.createFromAsset( getActivity().getAssets(), "fontawesome-webfont.ttf" );
+        close.setTypeface(font);
+        final PastContextSelectedFragment f = this;
+        close.setOnClickListener(new OnClickListener() {
+        	  @Override
+        	  public void onClick(View v) {
+        		  getActivity().getSupportFragmentManager().beginTransaction().remove(f).commit();
+        	  }
+        	});
+        return view;
+	}
+	
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+	  Dialog dialog = super.onCreateDialog(savedInstanceState);
+
+	  // request a window without the title
+	  dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+	  return dialog;
 	}
 	
 
